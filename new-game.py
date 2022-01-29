31daaf9a1da99717b956068a9b2d7809bf49244b
KirisameKei <53395204+KirisameKei@users.noreply.github.com>
2022-01-29 21:26:07 +0900

    UNO機能を追加

--- new-game.py
+++ new-game.py
@@ -1,6 +1,7 @@
 import datetime
 import json
 import os
+import random
 import traceback
 
 import discord
@@ -11,6 +12,7 @@ import othello
 import ox
 import puzzle15
 import syogi
+import uno
 
 intents = discord.Intents.all()
 client3 = discord.Client(intents=intents)
@@ -21,6 +23,7 @@ where_from = os.getenv("where_from")
 about_ox = [] #[int, datetime.datetime, discord.Member, discord.Member]
 about_othello = [] #[int, datetime.datetime, discord.Member, discord.Member]
 about_syogi = [] #[datetime.datetime, discord.Member, discord.Member]
+about_uno = [] #[datetime.datetime, discord.Message, bool, discord.Member × n]
 about_puzzle15 = [] #[discord.Member]
 
 def unexpected_error():
@@ -58,13 +61,8 @@ async def on_message(message):
             if not message.author.bot:
                 await message.channel.send(where_from)
 
-        if message.guild is None:
-            for i in range(5):
-                await message.channel.send("DMに来るんじゃねぇ馬鹿野郎！")
-            return
-
-        if not message.channel.id == 691901316133290035: #ミニゲーム
-        #if not message.channel.id == 597978849476870153: #3組
+        #if not message.channel.id == 691901316133290035: #ミニゲーム
+        if not message.channel.id == 597978849476870153: #3組
             return
 
         if message.content.startswith("/ox"):
@@ -73,6 +71,8 @@ async def on_message(message):
             await start_othello(message)
         elif message.content == "/syogi":
             await start_syogi(message)
+        elif message.content == "/uno":
+            await start_uno(message)
         elif message.content == "/puzzle15":
             await start_puzzle15(message)
         elif message.content == "/cancel":
@@ -82,12 +82,19 @@ async def on_message(message):
         unexpected_error()
 
 
+def can_you_start_game(message):
+    if message.author in about_ox or message.author in about_othello or message.author in about_syogi or message.author in about_puzzle15 or message.author in about_uno:
+        return False
+    else:
+        return True
+
+
 async def start_ox(message):
     if len(about_ox) == 4: #他にプレイしている人がいたら
         await message.channel.send("現在プレイ中です。しばらくお待ちください。")
         return
 
-    if message.author in about_ox or message.author in about_othello or message.author in about_syogi or message.author in about_puzzle15:
+    if not can_you_start_game(message):
         await message.channel.send("あなたは別のゲームに参加しているか既に参加しているため参加できません")
         return
 
@@ -122,7 +129,7 @@ async def start_othello(message):
         await message.channel.send("現在プレイ中です。しばらくお待ちください。")
         return
 
-    if message.author in about_ox or message.author in about_othello or message.author in about_syogi or message.author in about_puzzle15:
+    if not can_you_start_game(message):
         await message.channel.send("あなたは別のゲームに参加しているか既に参加しているため参加できません")
         return
 
@@ -161,7 +168,7 @@ async def start_syogi(message):
         await message.channel.send("現在プレイ中です。しばらくお待ちください。")
         return
 
-    if message.author in about_ox or message.author in about_othello or message.author in about_syogi or message.author in about_puzzle15:
+    if not can_you_start_game(message):
         await message.channel.send("あなたは別のゲームに参加しているか既に参加しているため参加できません")
         return
 
@@ -176,8 +183,38 @@ async def start_syogi(message):
         await message.channel.send("他の参加者を待っています・・・")
 
 
+async def start_uno(message):
+    if not can_you_start_game(message):
+        await message.channel.send("あなたは別のゲームに参加しているか既に参加しているため参加できません")
+        return
+
+    if len(about_uno) == 0:
+        about_uno.append(datetime.datetime.now())
+        embed = discord.Embed(
+            title="UNO募集",
+            description=f"{message.author.mention}",
+            color=random.choice([0x0000ff, 0x00aa00, 0xff0000, 0xffff00])
+        )
+        msg = await message.channel.send(content="✋で参加、👋で退出、🆗で開始\n・UNOコール不要\n・ドローに重ねての回避不可\n・記号カードで上がれる\n・ドロー4のチャレンジなし```\nワイルド→WL, ドロー4→D4, 山札から引く→PL, その他→カードに記載```リアクションが全て付いてからアクションを行ってください\nメッセージ系タイムアウト1分, リアクション系タイムアウト30秒", embed=embed)
+        about_uno.append(msg)
+        await msg.add_reaction("✋")
+        await msg.add_reaction("👋")
+        await msg.add_reaction("🆗")
+
+        about_uno.append(False) #プレイ中のフラグ
+        about_uno.append(message.author)
+
+    elif not about_uno[2]: #募集中なら
+        await message.channel.send(f"現在{about_uno[3].name}により募集されています")
+        return
+
+    elif about_uno[2]: #プレイ中なら
+        await message.channel.send("現在プレイ中です。しばらくお待ちください。")
+        return
+
+
 async def start_puzzle15(message):
-    if message.author in about_ox or message.author in about_othello or message.author in about_syogi or message.author in about_puzzle15:
+    if not can_you_start_game(message):
         await message.channel.send("あなたは別のゲームに参加しているか既に参加しているため参加できません")
         return
 
@@ -222,9 +259,87 @@ async def cancel(message):
 async def on_ready():
     try:
         ch = client3.get_channel(597978849476870153)
-        await ch.send(f"{client3.user.name}がログインしました(from: {where_from})")
+        with open("./version.txt", mode="r", encoding="utf-8") as f:
+            version = f.read()
+        await ch.send(f"{client3.user.name}がログインしました(from: {where_from})\nversion: {version}\n本番リリース時on_messageとloopのchを3組からミニゲームに変更")
     except:
         unexpected_error()
+
+
+@client3.event
+async def on_reaction_add(reaction, user):
+    if user.bot:
+        return
+
+    if len(about_uno) == 0:
+        return
+
+    if about_uno[2]:
+        return
+
+    if reaction.message == about_uno[1]:
+        msg = about_uno[1]
+        player = reaction.message.guild.get_member(user.id)
+        if reaction.emoji == "✋":
+            if player in about_uno:
+                await reaction.remove(user)
+                return
+            about_uno.append(player)
+            description = ""
+            for mem in about_uno[3:]:
+                description += f"{mem.mention}\n"
+            embed = discord.Embed(
+                title="UNO募集",
+                description=description,
+                color=msg.embeds[0].color
+            )
+            await msg.edit(embed=embed)
+            await reaction.remove(user)
+        elif reaction.emoji == "👋":
+            if not (player in about_uno):
+                await reaction.remove(user)
+                return
+            about_uno.remove(player)
+            if len(about_uno[3:]) == 0:
+                embed = discord.Embed(
+                    title="募集終了",
+                    description="参加者が全員退出したため募集は終了されました",
+                    color=0x000000
+                )
+                await msg.edit(embed=embed)
+                await msg.clear_reactions()
+                about_uno.clear()
+                return
+            description = ""
+            for mem in about_uno[3:]:
+                description += f"{mem.mention}\n"
+            embed = discord.Embed(
+                title="UNO募集",
+                description=description,
+                color=msg.embeds[0].color
+            )
+            await msg.edit(embed=embed)
+            await reaction.remove(user)
+        elif reaction.emoji == "🆗":
+            if not(player in about_uno):
+                await reaction.remove(user)
+                return
+            if len(about_uno[3:]) == 1:
+                await msg.channel.send("1人でUNOする気ですか？させませんよ", delete_after=3)
+                await reaction.remove(user)
+                return
+            about_uno[2] = True
+            embed = discord.Embed(
+                title="**募集終了**",
+                description=msg.embeds[0].description,
+                color=msg.embeds[0].color
+            )
+            await msg.edit(content=f"{user.name}がゲームを開始しました", embed=embed)
+            await msg.clear_reactions()
+            await uno.match_uno(client3, msg, about_uno)
+        else:
+            await reaction.remove(user)
+
 
 
 @tasks.loop(seconds=60)
@@ -232,8 +347,8 @@ async def loop():
     await client3.wait_until_ready()
 
     before_30min = datetime.datetime.now() - datetime.timedelta(minutes=30)
-    ch = client3.get_channel(691901316133290035) #ミニゲーム
-    #ch = client3.get_channel(597978849476870153) #3組
+    #ch = client3.get_channel(691901316133290035) #ミニゲーム
+    ch = client3.get_channel(597978849476870153) #3組
     if len(about_ox) == 3:
         if about_ox[1] <= before_30min:
             member = about_ox[2]
@@ -251,6 +366,11 @@ async def loop():
             member = about_syogi[1]
             about_syogi.clear()
             await ch.send(f"{member.mention}\n30分間参加がなかったので募集は取り消されました")
+
+    if len(about_uno) == 4:
+        if about_uno[0] <= before_30min:
+            about_uno.clear()
+            await ch.send("30分間参加がなかったので募集は取り消されました")
 
 loop.start()
 
